4c3e48125e55e80cd1b5755eae023b8f36e9e516
AlfianKamil9 <rifqialfiansyahk@gmail.com>
2023-07-12 22:29:28 +0700

    merging

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::post('/logout', [LoginController::class, 'logout']);
-});
+// */
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('/user', function (Request $request) {
+//         return $request->user();
+//     });
+//     Route::post('/logout', [LoginController::class, 'logout']);
+// });
 
-Route::post('/login', [LoginController::class, 'index'])->name('login');
+// Route::post('/login', [LoginController::class, 'index'])->name('login');
