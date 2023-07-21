6f572211070709e1b9f302a77bf683e3b0280432
AlfianKamil9 <rifqialfiansyahk@gmail.com>
2023-07-21 14:19:08 +0700

    mengelompokkan view login dan register ke pages/auth

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -12,50 +12,7 @@
 @section('content')
 
 <div class="container p-5">
-      <div class="row align-items-center justify-content-around">
-        
-        {{-- <x-input-error :messages="$errors->get('password')" class="mt-2 fst-italic text-danger" style="font-size: 14px" /> --}}
-          {{-- <form > 
-            @csrf
-          <form>
-            <div class="mb-4">
-              <input
-                type="email"
-                name="email"
-                class="form-control border-dark rounded-0 px-2 py-3"
-                id="email"
-                value="{{ old('email') }}"
-                placeholder="Email" required />
-            </div>
-            <div class="mb-4">
-              <input
-                type="password"
-                name="password"
-                class="form-control border-dark rounded-0 px-2 py-3"
-                id="password"
-                placeholder="Password" required/>
-            </div>
-            <div class="mb-3 text-end">
-              <a href="{{ route('password.request') }}" class="link-secondary text-decoration-none"
-                >Forgot password?</a
-              >
-            </div>
-            <div class="d-flex flex-column mt-auto">
-              <button
-                style="background: #d2e122"
-                type="submit"
-                class="btn btn-success fw-semibold mb-2 py-3 rounded-4 w-75 align-self-center border-0 text-dark">
-                Login
-              </button>
-              <a href="{{ route('auth.google') }}" type="btn" class="btn fw-semibold mb-2">
-                <img src="assets/img/google.png" alt="" /> Sign in with Google
-              </a>
 
-              <a href="" class="btn fw-semibold text-decoration-none">Signup Now</a>
-
-            </div>
-          </form> --}}
-        </div> 
 <div class="container-md pt-5 mt-5 px-4">
     <div class="row align-items-center justify-content-center gap-2">
       <!-- Left Column -->
