34256a6a9e15411067cdcbe0a00b0a4dde565667
Melvin Marcello <melvinmarcello2978@gmail.com>
2023-07-24 21:04:31 +0700

    minor-changes

--- resources/views/pages/psikolog.blade.php
+++ resources/views/pages/psikolog.blade.php
@@ -1,23 +1,26 @@
 @extends('../layout')
 
-@section('title', 'Login')
+@section('title', 'Daftar Psikolog')
 
 @section('styles')
     <link rel="stylesheet" href="assets/css/psikolog-page.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 @endsection
+
+{{-- Nav --}}
+@include('../partials/navbar') 
 
 @section('content')
     <section id="psikologi" class="py-5">
         <div class="container">
-            <div class="form-container">
-                <label class="text-medium" for="">Masalah yang dihadapi</label>
+            <div class="form-container ms-5">
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control form-masalah" placeholder="masukan masalah" aria-label="Username" aria-describedby="basic-addon1">
+                    <input type="text" class="form-control form-masalah" placeholder="Masalah yang dihadapi" aria-label="Username" aria-describedby="basic-addon1">
                     <span class="input-group-text" id="basic-addon1"><i class="bi bi-arrow-right"></i></span>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-xl-6">
+            <div class="row justify-content-center gap-1">
+                <div class="col-xl-5">
                     <div class="card-container py-5">
                         <img src="https://placehold.co/300" alt="" class="psikolog-image">
                         <h3 class="text-center mt-3">Nama</h3>
@@ -45,7 +48,7 @@
                         <button class="button-konsultasi mx-auto">Jadwalkan Konsultasi</button>
                     </div>
                 </div>
-                <div class="col-xl-6">
+                <div class="col-xl-5">
                     <div class="card-container py-5">
                         <img src="https://placehold.co/300" alt="" class="psikolog-image">
                         <h3 class="text-center mt-3">Nama</h3>
