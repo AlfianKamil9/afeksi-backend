381becf30c4267c9e1c10d4f84e09fd44d8d76b4
Jonathan Pangkey <gloriaestefania@Glorias-MacBook-Air-M1.local>
2023-07-25 16:45:46 +0800

    update desain footer

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 <div class="container-lg pt-5 mt-5 px-4">
     <div class="row align-items-center justify-content-center gap-2">
-     ]
+     
       <div class="col-md-5 d-none d-md-inline">
         <img src="assets/img/login-register/loginImg.svg" class="img-fluid align-self-center" alt="Image">
         <div class="heading text-center mt-5">
