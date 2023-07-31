767d83867aa1be09b0bce1378f690df6ec2960b5
Lucasjan3123 <lucasjanmalvin@gmail.com>
2023-07-30 22:57:46 -0700

    UPDATE

--- app/Http/Controllers/Transaksi/Event/WebinarTransaksiController.php
+++ app/Http/Controllers/Transaksi/Event/WebinarTransaksiController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Models\EventTransaction;
 use Illuminate\Support\Facades\Validator;
 use App\Services\Midtrans\PembayaranEvent\TransferBankService;
+use App\Services\Midtrans\PembayaranEvent\CstoreService;
+
 
 class WebinarTransaksiController extends Controller
 {
@@ -130,12 +132,78 @@ class WebinarTransaksiController extends Controller
 
         else if ($request->payment_method == 'alfamart' ) 
             {
-                # code...
+                $data = EventTransaction::with('event', 'user')->where('ref_transaction_event', $request->reference)->first();
+                $data = [
+                    "reference" => $request->reference,
+                    "harga_event" => $data->event->price_event,
+                ];
+                
+                $result = new CstoreService();
+                $res = $result-> alfamart($request->payment_method, $data);
+
+              // Konversi respons menjadi array
+            $resArray = json_decode($res, true);
+
+            // Periksa apakah konversi berhasil dan $resArray adalah array
+            if (is_array($resArray) && isset($resArray["status_code"]) && $resArray["status_code"] != 201) {
+                //CEK RESPON ORDER
+                if ($resArray["order_id"] != $data["reference"]) {
+                    return response()->json(["message" => "Sorry, Your Order Id is not valid"]);
+                }
+
+                // SET UPDATE TABLE TRANSAKSI EVENT
+                EventTransaction::where('ref_transaction_event', $resArray["order_id"])->update([
+                    "payment_method" => $request->payment_method,
+                    "total_payment" => $resArray["gross_amount"],
+                    "fee_transaction" => 4000,
+                    "status" => "PENDING",
+                    "updated_at" => $resArray["transaction_time"]
+                ]);
+
+                return response()->json([
+                    "message" =>  $resArray["status_message"],
+                    "store" => $resArray["store"],
+                    "payment_code" => $resArray["payment_code"]
+                ]);
+            }
+             else {
+                // Tangani jika konversi gagal atau status_code == 201
+                return response()->json(["message" => "Unexpected response format or status code is 201"]);
+            }; 
             }
 
         else if ($request->payment_method == 'indomaret') 
             {
-                # code...
+                $data1 = EventTransaction::with('event', 'user')->where('ref_transaction_event', $request->reference)->first();
+                $data = [
+                    "reference" => $request->reference,
+                    "harga_event" => $data1->event->price_event,
+                ];
+                
+                $result = new CstoreService();
+                $res = $result-> indomaret($request->payment_method, $data);
+
+                //CEK KODE RESPON
+                if ($res["status_code"] != 201 ) {
+                    return response()->json($res);
+                }
+                //CEK RESPON ORDER
+                if ($res["order_id"] != $data["reference"]) {
+                    return response()->json(["message" => "Sorry, Yor Order Id and not valid"]);
+                }
+                // SET UPDATE TABLE TRANSAKSI EVENT
+                EventTransaction::where('ref_transaction_event', $res["order_id"])->update([
+                    "payment_method" => $request->payment_method,
+                    "total_payment" => $res["gross_amount"],
+                    "fee_transaction" => 4000,
+                    "status" => "PENDING",
+                    "updated_at" => $res["transaction_time"]
+                ]);
+                return response()->json([
+                    "message" =>  $res["status_message"],
+                    "store" => $res["store"],
+                    "payment_code" => $res["payment_code"]
+                ]);
             }
         else if ($request->payment_method == 'qris') 
             {
