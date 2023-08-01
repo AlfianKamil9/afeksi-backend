356e7db4c52f193a55ff6ebd73fb8782ec7179e5
Tiara <tiarafirdausa94@gmail.com>
2023-08-01 11:34:24 +0700

    list+detail+filter webinar

--- app/Http/Controllers/Event/WebinarController.php
+++ app/Http/Controllers/Event/WebinarController.php
@@ -2,27 +2,94 @@
 
 namespace App\Http\Controllers\Event;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\EventMaterialSession;
 
 class WebinarController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = EventMaterialSession::query()
+        ->with(['pembicara'])
+        ->join('events', 'event_material_sessions.event_id', '=', 'events.id')
+        ->where('activity_category_event', 'WEBINAR');
+
+        // Filter using input type text
+        if ($request->has('input_search')) {
+            $query->where('title_event', 'like', '%' . $request->input('input_search') . '%');
+        }
+
+        // Filter by date using dropdown
+        $dateFilter = $request->input('sort_date');
+        if ($dateFilter === 'latest') {
+            $query->orderBy('date_event', 'desc');
+        } elseif ($dateFilter === 'oldest') {
+            $query->orderBy('date_event', 'asc');
+        }
+
+        // Filter by minimum and maximum price using input fields
+        $minPrice = $request->input('min_price');
+        $maxPrice = $request->input('max_price');
+        if ($minPrice !== null) {
+            $query->where('price_event', '>=', $minPrice);
+        }
+        if ($maxPrice !== null) {
+            $query->where('price_event', '<=', $maxPrice);
+        }
+
+        // filter by category_event_id using checkbox
+        if ($request->has('category_event_id')) {
+            $query->where('category_event_id', $request->input('category_event_id'));
+        }
+
+        // filter by time_category_event using checkbox
+        if ($request->has('time_category_event')) {
+            $query->where('time_category_event', $request->input('time_category_event'));
+        }
+
+        // filter by pay_category_event using checkbox
+        if ($request->has('pay_category_event')) {
+            $query->where('pay_category_event', $request->input('pay_category_event'));
+        }
+
+        $data = $query->get();
+
+        foreach ($data as $event) {
+            $event->time_start = Carbon::parse($event->time_start)->format('H:i');
+            $event->time_finish = Carbon::parse($event->time_finish)->format('H:i');
+            $event->date_event = Carbon::parse($event->date_event)->format('d F Y');
+            // dd($event->time_start, $event->time_finish, $event->date_event);
+        }
+
+        return view('pages.kegiatan-webinar', [
+            'data' => $data,
+        ]);
     }
 
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($slug)
     {
-        //
+        $data = EventMaterialSession::query()
+        ->with(['pembicara'])
+        ->join('events', 'event_material_sessions.event_id', '=', 'events.id')
+        ->where('events.slug_event', $slug)
+        ->firstOrFail();
+
+        $data->time_start = Carbon::parse($data->time_start)->format('H:i');
+        $data->time_finish = Carbon::parse($data->time_finish)->format('H:i');
+        $data->date_event = Carbon::parse($data->date_event)->format('d F Y');
+
+        // dd($data);
+        return view('pages.detail-webinar',[
+            'data' => $data
+        ]);
     }
-
-
 }
--- resources/views/pages/detail-webinar.blade.php
+++ resources/views/pages/detail-webinar.blade.php
@@ -1,125 +1,142 @@
 @extends('../layout')
 
-@section('title', 'Webinar | AFEKSI')
+@section('title')
+    {{ $data->title_event }} | AFEKSI
+@endsection
 
 <!-- path style disesuaikan dengan folder css masing-masing page -->
 @section('styles')
-    <link rel="stylesheet" href="assets/css/detail-kegiatan.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/detail-kegiatan.css') }}">
 @endsection
 
 {{-- @include('../partials/navbar')  --}}
 
 @section('content')
-
-<div class="hero campaign" style="background-image: url(assets/img/kegiatan/Banner.svg)">
-      <div class="d-flex justify-content-between flex-column flex-lg-row gap-5 text-center text-lg-start">
-        <div class="left col-lg-8">
-          <div class="d-flex dflex gap-2 text-white ">
-            <p>Kegiatan</p>
-            <p><img src="assets/img/kegiatan/Vector (6).png" class="img-fluid" alt="" /></p>
-            <p>Campaign</p>
-            <p><img src="assets/img/kegiatan/Vector (6).png" class="img-fluid" alt="" /></p>
-            <p>Love Yourself Before Loving Others</p>
-          </div>
-          <div class="text text-white mt-5">
-            <h1 class="mb-4">Love Yourself Before Loving Others</h1>
-            <p>
-              Jangan lewatkan kesempatan untuk belajar dari para ahli, mendapatkan wawasan baru, dan berinteraksi dengan orang-orang lain yang memiliki minat yang sama. Bersama-sama, mari kita bangun hubungan yang lebih mendalam, intim, dan
-              penuh makna!
-            </p>
-          </div>
+    {{-- Header --}}
+    <div class="hero webinar" style="background-image: url(../assets/img/kegiatan/Banner.svg)">
+        <div class="d-flex justify-content-between flex-column flex-lg-row gap-5 text-center text-lg-start">
+            <div class="left col-lg-8">
+                <div class="d-flex dflex gap-2 text-white ">
+                    <p>Kegiatan</p>
+                    <p><img src="{{ asset('assets/img/kegiatan/Vector (6).png') }}" class="img-fluid" alt="" /></p>
+                    <p><a href="{{ route("webinar") }}" class="text-light">Webinar</a></p>
+                    <p><img src="{{ asset('assets/img/kegiatan/Vector (6).png') }}" class="img-fluid" alt="" /></p>
+                    <p><a href="/kegiatan-webinar/{{ $data->slug_event  }}" class="text-light">{{ $data->title_event }}</a></p>
+                </div>
+                <div class="text text-white mt-5">
+                    <h1 class="mb-4">{{ $data->title_event }}</h1>
+                    <p>
+                    Jangan lewatkan kesempatan untuk belajar dari para ahli, mendapatkan wawasan baru, dan berinteraksi dengan orang-orang lain yang memiliki minat yang sama. Bersama-sama, mari kita bangun hubungan yang lebih mendalam, intim, dan
+                    penuh makna!
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
 
+    {{-- Cover Webinar --}}
     <div class="card-daftar col-lg-3">
-      <div class="card border-0 rounded-4 shadow">
-        <img src="assets/img/kegiatan-detail-webinar/hero1.svg" class="card-img-top" alt="Hero " />
-        <div class="card-body">
-          <h4 class="card-title fw-bold mb-4" style="color: #2139f9">Gratis</h4>
-          <div class="d-grid gap-2">
-            <a href="#" class="btn text-white btn-daftar" type="button" style="background-color: #2139f9">Daftar Sekarang</a>
-          </div>
-          <h6 class="fw-bold mt-3">Keuntungan yang kamu dapetin:</h6>
-          <ul class="custom-list list-unstyled m-1 text-muted">
-            <li class="mb-1 gap-3"><img src="assets/img/kegiatan-detail-webinar/certificate.svg" alt="E-Certificate" /> E-Certificate</li>
-            <li class="mb-1"><img src="assets/img/kegiatan-detail-webinar/softcopy.svg" alt="SoftCopy" /> Softcopy Material</li>
-            <li class="mb-1"><img src="assets/img/kegiatan-detail-webinar/knowledge.svg" alt="Knowledge" /> Knowledge</li>
-            <li class="mb-1"><img src="assets/img/kegiatan-detail-webinar/doorprize.svg" alt="Doorprize" /> Doorprize</li>
-            <li class="mb-1"><img src="assets/img/kegiatan-detail-webinar/diskusi.svg" alt="Diskusi" /> Diskusi</li>
-          </ul>
-          <h6 class="fw-bold mt-3">Bahasa Pengantar</h6>
-          <p class="text-muted">Bahasa Indonesia</p>
+        <div class="card border-0 rounded-4 shadow">
+            <img src="{{ asset('/assets/img/kegiatan/'.$data->cover_event) }}" class="card-img-top" alt="{{ $data->title_event }}" />
+            <div class="card-body">
+                <h4 class="card-title fw-bold mb-4" style="color: #2139f9">
+                    @if ($data->pay_category_event != "FREE")
+                        Rp{{ number_format($data->price_event, 0, ',', '.') }}
+                    @else
+                        Gratis
+                    @endif
+                </h4>
+                <form>
+                    <div class="d-grid gap-2">
+                        <a href="#" class="btn text-white btn-daftar" type="button" style="background-color: #2139f9">Daftar Sekarang</a>
+                    </div>
+                </form>
+                <h6 class="fw-bold mt-3">Keuntungan yang kamu dapetin:</h6>
+                <ul class="custom-list list-unstyled m-1 text-muted">
+                    <li class="mb-1 gap-3"><img src="{{ asset('assets/img/kegiatan-detail-webinar/certificate.svg') }}" alt="E-Certificate" /> E-Certificate</li>
+                    <li class="mb-1"><img src="{{ asset('assets/img/kegiatan-detail-webinar/softcopy.svg') }}" alt="SoftCopy" /> Softcopy Material</li>
+                    <li class="mb-1"><img src="{{ asset('assets/img/kegiatan-detail-webinar/knowledge.svg') }}" alt="Knowledge" /> Knowledge</li>
+                    <li class="mb-1"><img src="{{ asset('assets/img/kegiatan-detail-webinar/doorprize.svg') }}" alt="Doorprize" /> Doorprize</li>
+                    <li class="mb-1"><img src="{{ asset('assets/img/kegiatan-detail-webinar/diskusi.svg') }}" alt="Diskusi" /> Diskusi</li>
+                </ul>
+                <h6 class="fw-bold mt-3">Bahasa Pengantar</h6>
+                <p class="text-muted">Bahasa Indonesia</p>
+            </div>
         </div>
-      </div>
     </div>
 
     <!-- Tab Bar -->
     <div class="container">
-      <div class="d-flex flex-row mb-3">
-        <div class="p-2"><a href="">Deskripsi</a></div>
-        <div class="p-2"><a href="" class="text-muted">Detail Kegiatan</a></div>
-        <div class="p-2"><a href="" class="text-muted">Cara Mengikuti Webinar</a></div>
-        <div class="p-2"><a href="" class="text-muted">Profil Pembicara</a></div>
-      </div>
+        <div class="d-flex flex-row mb-3">
+            <div class="p-2"><a href="#deskripsi">Deskripsi</a></div>
+            <div class="p-2"><a href="#detail-kegiatan" class="text-muted">Detail Kegiatan</a></div>
+            <div class="p-2"><a href="#cara-mengikuti" class="text-muted">Cara Mengikuti Webinar</a></div>
+            <div class="p-2"><a href="#profil-pembicara" class="text-muted">Profil Pembicara</a></div>
+        </div>
     </div>
     <!-- End Tab Bar -->
     <hr class="mt-4" />
 
-    <!-- DESKRIPSI -->
     <div class="container">
-      <div class="row">
-        <div class="col-lg-8">
-          <h5 class="fw-bold">Deskripsi</h5>
-          <p style="text-align: justify">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem
-            Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-        </div>
-      </div>
-      <!-- PROFIL PEMBACA -->
-      <div class="row">
-        <div class="col-lg-8">
-          <h5 class="fw-bold mb-3">Profil Pembicara</h5>
-          <div class="d-flex">
-            <div class="flex-shrink-0">
-              <img src="assets/img/kegiatan-detail-webinar/people.svg" alt="Foto Profil Pembaca" />
+        <!-- DESKRIPSI -->
+        <div class="row" id="deskripsi">
+            <div class="col-lg-8">
+                <h5 class="fw-bold">Deskripsi</h5>
+                <p style="text-align: justify">
+                    {!! $data->description_event !!}
+                </p>
             </div>
-            <div class="flex-grow-1 ms-3 mb-5">
-              <h6 class="fw-bold">Heraldha Savira, Dip. ABRSM, S.Psi</h6>
-              <p class="text-muted mb-0">Clinical Physcology Grads</p>
-              <p class="text-muted mb-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab magni expedita quam voluptate suscipit commodi?</p>
+        </div>
+
+        <!-- PROFIL PEMBICARA -->
+        <div class="row" id="profil-pembicara">
+            <div class="col-lg-8">
+            <h5 class="fw-bold mb-3">Profil Pembicara</h5>
+            <div class="d-flex">
+                <div class="flex-shrink-0">
+                    <img src="{{ asset('assets/img/kegiatan-detail-webinar/people.svg') }}" alt="Foto Profil Pembaca" />
+                </div>
+                <div class="flex-grow-1 ms-3 mb-5">
+                    <h6 class="fw-bold">{{ $data->pembicara->nama_psikolog }}</h6>
+                    <p class="text-muted mb-0">{{ $data->pembicara->profil }}</p>
+                    <p class="text-muted mb-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab magni expedita quam voluptate suscipit commodi?</p>
+                </div>
             </div>
-          </div>
+            </div>
         </div>
-      </div>
-      <!-- DETAIL KEGIATAN -->
-      <div class="row">
-        <div class="col-lg-8">
-          <h5 class="fw-bold mb-3">Detail Kegiatan</h5>
-          <p>Webinar ini akan dilaksanakan pada :</p>
-          <ul class="custom-list list-unstyled m-1 text-muted">
-            <li class="mb-2 gap-3"><img src="assets/img/kegiatan-detail-webinar/kalender.svg" width="21" height="23" alt="Tanggal Kegiatan" /> Minggu, 12 Februari 2023</li>
-            <li class="mb-2"><img src="assets/img/kegiatan-detail-webinar/location.svg" width="21" height="23" alt="Lokasi Kegiatan" /> Online Via Zoom</li>
-            <li class="mb-2"><img src="assets/img/kegiatan-detail-webinar/time.svg" width="21" height="23" alt="Waktu Kegiatan" /> 09.30 - 12.30 WIB</li>
-          </ul>
+
+        <!-- DETAIL KEGIATAN -->
+        <div class="row" id="detail-kegiatan">
+            <div class="col-lg-8">
+                <h5 class="fw-bold mb-3">Detail Kegiatan</h5>
+                <p>Webinar ini akan dilaksanakan pada :</p>
+                <ul class="custom-list list-unstyled m-1 text-muted">
+                    <li class="mb-2 gap-3"><img src="{{ asset('assets/img/kegiatan-detail-webinar/kalender.svg') }}" width="21" height="23" alt="Tanggal Kegiatan" /> {{ $data->date_event }}</li>
+                    <li class="mb-2"><img src="{{ asset('assets/img/kegiatan-detail-webinar/location.svg') }}" width="21" height="23" alt="Lokasi Kegiatan" />
+                    @if ( $data->time_category_event != "ONLINE")
+                    Offline Di {{ $data->is_place }}
+                    @else
+                    Online Via {{ $data->is_place }} </li>
+                    @endif
+                    <li class="mb-2"><img src="{{ asset('assets/img/kegiatan-detail-webinar/time.svg') }}" width="21" height="23" alt="Waktu Kegiatan" /> {{ $data->time_start }} - {{ $data->time_finish }} WIB</li>
+                </ul>
+            </div>
         </div>
-      </div>
-      <!-- CARA MENGIKUTI WEBINAR -->
-      <div class="row mt-5 mb-5">
-        <div class="col-lg-8">
-          <h5 class="fw-bold mb-3">Cara Mengikuti Webinar</h5>
-          <ol class="list-daftar-webinar">
-            <li class="mb-1">Daftar dan akses webinar di Afeksi melalui situs resmi.</li>
-            <li class="mb-1">Daftar dan akses webinar di Afeksi melalui situs resmi.</li>
-            <li class="mb-1">Daftar dan akses webinar di Afeksi melalui situs resmi.</li>
-            <li class="mb-1">Daftar dan akses webinar di Afeksi melalui situs resmi.</li>
-          </ol>
+
+        <!-- CARA MENGIKUTI WEBINAR -->
+        <div class="row mt-5 mb-5" id="cara-mengikuti">
+            <div class="col-lg-8">
+            <h5 class="fw-bold mb-3">Cara Mengikuti Webinar</h5>
+            <ol class="list-daftar-webinar">
+                <li class="mb-1">Daftar dan akses webinar di Afeksi melalui situs resmi.</li>
+                <li class="mb-1">Daftar dan akses webinar di Afeksi melalui situs resmi.</li>
+                <li class="mb-1">Daftar dan akses webinar di Afeksi melalui situs resmi.</li>
+                <li class="mb-1">Daftar dan akses webinar di Afeksi melalui situs resmi.</li>
+            </ol>
+            </div>
         </div>
-      </div>
     </div>
 
-@include('../partials/footer') 
+@include('../partials/footer')
 
 @endsection
