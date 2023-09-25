1800fa72a4edd6e1a18fa630f704da98d9cb6d32
AlfianKamil9 <rifqialfiansyahk@gmail.com>
2023-09-25 17:21:50 +0700

    dashboard profile

--- resources/views/pages/dashboard-profile.blade.php
+++ resources/views/pages/dashboard-profile.blade.php
@@ -1,4 +1,4 @@
-@extends('../layout')
+{{-- @extends('../layout')
 
 @section('title', 'Profile Dashboard | AFEKSI')
 
@@ -217,4 +217,136 @@
     
 
 @include('../partials/footer') 
+@endsection --}}
+
+@extends('../layout')
+@section('title', 'Profile Dashboard | AFEKSI')
+@section('styles')
+    <link rel="stylesheet" href="/assets/css/dashboard-profile.css">
 @endsection
+@section('content')
+<div class="row justify-content-end">    
+<div class="col-12 position-relative">
+    <!-- HERO
+      ================================================================= -->
+    <section>
+        <div class="hero d-flex">
+            <div class="container py-5 my-5 ">
+                <div class="row justify-content-end">
+                    <div class="col-md-9 d-flex flex-column">
+                        <h4 class="fw-semibold mt-5"><span>Welcome</span>, {{ Auth::user()->nama }}</h4>
+                        <p class="fs-5 fw-light mt-3">Untuk memudahkan proses konsultasi kamu, harap masukkan informasi yang valid.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Dashboard ============================== -->
+    <div class="dashboard p-3 shadow-sm">
+        <div class="bg-light rounded-circle d-flex justify-content-center align-items-center border" id="profile-foto" style="width: 125px; height: 125px;cursor:pointer;">
+            <svg data-src="/assets/img/dashboard-profile/user.svg" width="50px" height="50px"></svg>
+        </div>
+        <div class="mt-3" id="profile-foto" style="cursor:pointer;">
+            <h6 class="fw-bold">{{ Auth::user()->nama }}</h6>
+            <p>{{ Auth::user()->email }}</p>
+        </div>
+        
+        <div class="mb-4 mt-5 p-1  @if(Route::currentRouteName() == 'dashboard.index') active @endif "><a href="{{ route('dashboard.index') }}" class="text-secondary"><svg data-src="/assets/img/dashboard-profile/dashboard.svg" class="me-2" width="20" height="20" fill="#828282"></svg>Dashboard</a></div>
+        <div class="mb-4 @if(Route::currentRouteName() == 'dashboard.profile.index') active @endif p-1"><a href="{{ route('dashboard.profile.index') }}" class="text-secondary"><svg data-src="/assets/img/dashboard-profile/user.svg" class="me-2" width="20" height="20"></svg>Profile</a></div>
+        <div class="mb-4 p-1"><a href="#" class="text-secondary"><img src="/assets/img/dashboard-profile/voucher.svg" width="20" height="20" class="me-2">Voucher</a></div>
+        {{-- <div class="mb-4 p-1"><a href="#" class="text-secondary"><svg data-src="/assets/img/dashboard-profile/courses.svg" width="20" height="20" fill="#828282" class="me-2"></svg>My Courses</a></div> --}}
+        <div class="mb-4 @if(Route::currentRouteName() == 'dashboard.show.e-book') active @endif p-1"><a href="{{ route('dashboard.show.e-book') }}" class="text-secondary"><svg data-src="/assets/img/dashboard-profile/e-book.svg" width="20" height="20" fill="#828282" class="me-2"></svg>My E-Book</a></div>
+        <div class="mb-4 @if(Route::currentRouteName() == 'dashboard.show.rekap.transaksi') active @endif p-1"><a href="{{ route('dashboard.show.rekap.transaksi') }}" class="text-secondary"><img src="/assets/img/dashboard-profile/transaction.svg" alt="" width="20" height="20" fill="#828282" class="me-2">Rekap Transaksi</a></div>
+    </div>
+    </div>
+
+<!-- End of dashboard profile  ===================================-->
+  <div class="col-md-9">
+      <!-- FORM ===================== -->
+      <div class="container mt-5">
+        <form class="mx-3" method="POST" action="{{ route('dashboard.profile.changes.data') }}">
+          @csrf
+              <div class="row">
+              <div class="col-12">
+                  <div class="mb-3">
+                      <!-- Nama Lengkap -->
+                      <label for="namaLengkap" class="form-label">Nama Lengkap</label>
+                      <input type="text" class="form-control" id="namaLengkap" name="nama" placeholder="Masukkan Nama Lengkap" value="{{ Auth::user()->nama }}">
+                  </div>
+                <div class="mb-3">
+                      <!-- Email -->
+                      <label for="email" class="form-label">Email</label>
+                      <input type="email" class="form-control" id="email" name="email" placeholder="Masukkan Email" value="{{ Auth::user()->email }}">
+                </div>
+              </div>
+              <div class="col-6">
+                  <div class="mb-3">
+                      <label for="tanggalLahir" class="form-label">Tanggal Lahir</label>
+                      <div class="input-group">
+                        <input type="text" class="form-control" id="tanggalLahir" name="tgl_lahir" placeholder="Pilih Tanggal Lahir" value="{{ Auth::user()->tgl_lahir }}">
+                          <span class="input-group-text" data-toggle="datepicker" data-input="#tanggalLahir">
+                              <i class="bi bi-calendar"></i>
+                          </span>
+                      </div>
+                  </div>
+                  <div class="mb-3">
+                      <label for="noHP" class="form-label">No. HP</label>
+                      <input type="tel" class="form-control" id="noHP" placeholder="Masukkan No. HP" name="no_whatsapp" value="{{ Auth::user()->no_whatsapp }}">
+                  </div>
+                  <div class="mb-3">
+                      <label for="pekerjaan" class="form-label">Pekerjaan</label>
+                      <input type="text" class="form-control" id="pekerjaan" placeholder="Pekerjaan Kamu" name="pekerjaan" value="{{ Auth::user()->pekerjaan }}">
+                  </div>
+                  <div class="my-5 pb-5">
+                      <a type="button" href="{{ route('dashboard.profile.show.changePassword') }}" class="btn btn-outline-primary">Ubah Password</a>
+                      <a type="button" href="{{ route('dashboard.profile.show.changeFoto') }}" class="btn btn-outline-primary">Ubah Foto Profile</a>
+                  </div>
+                  
+              </div>
+              <div class="col-6">
+                  <div class="mb-3">
+                      <label for="jenisKelamin" class="form-label">Jenis Kelamin</label>
+                      <select class="form-select" id="jenisKelamin" name="jenisKelamin">
+                          <option hidden>Pilihan</option>
+                          <option value="Laki-laki" @if (Auth::user()->jenisKelamin == 'Laki-Laki')
+                              selected
+                          @endif>Laki-laki</option>
+                          <option value="Perempuan" @if (Auth::user()->jenisKelamin == 'Perempuan')
+                              selected
+                          @endif>Perempuan</option>
+                      </select>
+                  </div>
+                  <div class="mb-3">
+                      <label for="kota" class="form-label">Kota</label>
+                      <input type="text" class="form-control" id="kota" name="domisili" placeholder="Masukkan Kota" value="{{ Auth::user()->domisili }}">
+                  </div>
+                  <div class="mb-3">
+                      <label for="instansi" class="form-label">Instansi</label>
+                      <input type="text" class="form-control" id="instansi" name="institusi" placeholder="Instansi Kamu" value="{{ Auth::user()->institusi }}">
+                  </div>
+                  <div class="my-5 pb-5">
+                      <button type="submit" class="btn btn-primary w-100">Simpan Perubahan</button>
+                  </div>
+                </div>
+              </div>
+          </form>
+      </div>
+  </div>
+</div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            flatpickr("#tanggalLahir", {
+                dateFormat: "Y-m-d", // Format tanggal yang diinginkan
+                locale: "id", // Lokal Bahasa Indonesia
+                altInput: true, // Menggunakan input alternatif yang mudah diisi
+                altFormat: "j F Y", // Format input alternatif
+                allowInput: true, // Memungkinkan pengguna menginputkan tanggal langsung
+                enableTime:false
+            });
+        });
+    </script>
+    
+    
+    
+@include('../partials/footer') 
+@endsection            
