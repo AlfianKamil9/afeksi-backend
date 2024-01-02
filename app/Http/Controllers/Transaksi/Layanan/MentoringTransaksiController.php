79991cfce063e0826d4aaa644500404b5c8a4b29
roihan365 <bitcoiner.banjarmasin@gmail.com>
2024-01-02 19:35:19 +0800

    fix error pembayaran midtrans

--- app/Http/Controllers/Transaksi/Layanan/MentoringTransaksiController.php
+++ app/Http/Controllers/Transaksi/Layanan/MentoringTransaksiController.php
@@ -65,7 +65,7 @@ class MentoringTransaksiController extends Controller
         } elseif ($tglSekarang == $tglKonsultasi && $blnSekarang > $blnKonsultasi && ($thnSekarang == $thnKonsultasi || $thnSekarang > $thnKonsultasi)) {
             Alert::alert()->html('<h4 class="text-danger fw-bold">Error</h4>', '<p>Invalid data, Please Filled Correctly!</p>');
             return back();
-        } elseif($request->jenisKelamin == 0){
+        } elseif ($request->jenisKelamin == 0) {
             Alert::alert()->html('<h4 class="text-danger fw-bold">Error</h4>', '<p>Invalid data, Mohon isi Jenis Kelamin Anda!</p>');
             return back();
         }
@@ -124,8 +124,7 @@ class MentoringTransaksiController extends Controller
         if (isset($request->btnBatalVoucher)) {
             session()->forget('apply');
             return back();
-        }
-        else if (isset($request->btnApplyVoucher)) {
+        } else if (isset($request->btnApplyVoucher)) {
             if (isset($request->input_code)) {
                 $now = Carbon::now();
                 $kode = Voucher::where('kode', $request->input_code)->where('expired_date', '>=', $now)->where('stok_voucher', '>', 0)->first();
@@ -155,11 +154,11 @@ class MentoringTransaksiController extends Controller
         $response = $this->klasifikasiPaymentMethod($bank, $ref_transaction_layanan, $voucher_id);
         // ---------------
         //dd($response);
-        if($response["status_code"] != 201 ) {
+        if ($response["status_code"] != 201) {
             Alert::alert()->html('<h4 class="fw-bold text-danger">FAILED PAYMENT METHOD</h4>', '<p>There was some error in the system, Please try again later or change the Payment Method.<br><br>Error: <span class="pt-2 fw-bold ">500 Server Error</span></p>');
             return back();
         }
-        if($voucher_id != null ) {
+        if ($voucher_id != null) {
             $stok = Voucher::where('kode', $request->input_code)->pluck('stok_voucher')->first();
             Voucher::where('id', $voucher_id)->update([
                 'stok_voucher' => $stok - 1
@@ -171,36 +170,31 @@ class MentoringTransaksiController extends Controller
         if ($bank == 'bni' || $bank == 'bri' || $bank == 'bca' || $bank == 'cimb' || $bank == 'permata') {
             $va = '<h6 style="text-transform:uppercase;">' . $bank . ' VA = ' . $getData->kode_bayar_1 . '</h6>';
             $pesan = "Silahkan Lengkapi Pembayaran Sebelum <br><strong>" . $getData2->updated_at->addDay(1)->format('l, d M Y') . "</strong> pukul </strong>" . $getData->updated_at->format('H:i') . "</strong>";
-        } 
-        else if ($bank == 'mandiri') {
+        } else if ($bank == 'mandiri') {
             $va =   '<h6 style="text-transform:uppercase;">' . $bank . ' Bill Key = ' . $getData->kode_bayar_1 . '</h6>
                     <h6 style="text-transform:uppercase;">' . $bank . ' Bill Code = ' . $getData->kode_bayar_2 . '</h6>';
             $pesan = "Silahkan Lengkapi Pembayaran Sebelum <br><strong>" . $getData2->updated_at->addDay(1)->format('l, d M Y') . "</strong> pukul </strong>" . $getData->updated_at->format('H:i') . "</strong>";
-        } 
-        else if ($bank == 'indomaret') {
+        } else if ($bank == 'indomaret') {
             $va =   '<h6 style="text-transform:uppercase;">' . $bank . ' Kode Pembayaran = ' . $getData->kode_bayar_1 . '<br> Kode Merchant = ' . $getData->kode_bayar_2 . '</h6>';
             $pesan = "Silahkan Lengkapi Pembayaran Sebelum <br><strong>" . $getData2->updated_at->addDay(1)->format('l, d M Y') . "</strong> pukul </strong>" . $getData->updated_at->format('H:i') . "</strong>";
-        }
-        else if ($bank == 'alfamart') {
+        } else if ($bank == 'alfamart') {
             $va =   '<h6 style="text-transform:uppercase;">' . $bank . ' Kode Pembayaran = ' . $getData->kode_bayar_1 . '</h6>';
             $pesan = "Silahkan Lengkapi Pembayaran Sebelum <br><strong>" . $getData2->updated_at->addDay(1)->format('l, d M Y') . "</strong> pukul </strong>" . $getData->updated_at->format('H:i') . "</strong>";
-        }
-        else if ($bank == 'shopeepay') {
-            $va =   '<center><a style="text-transform:uppercase;" href="'.$getData->kode_bayar_1.'" class="btn btn-primary">Bayar Sekarang</a></center>';
-            $pesan = "Silahkan Lengkapi Pembayaran Sebelum <br><strong>".$getData2->updated_at->format('l, d M Y')."</strong> pukul </strong>".$getData->updated_at->addMinutes(15)->format('H:i')."</strong>";
-        }
-        else if ($bank == 'gopay') {
+        } else if ($bank == 'shopeepay') {
+            $va =   '<center><a style="text-transform:uppercase;" href="' . $getData->kode_bayar_1 . '" class="btn btn-primary">Bayar Sekarang</a></center>';
+            $pesan = "Silahkan Lengkapi Pembayaran Sebelum <br><strong>" . $getData2->updated_at->format('l, d M Y') . "</strong> pukul </strong>" . $getData->updated_at->addMinutes(15)->format('H:i') . "</strong>";
+        } else if ($bank == 'gopay') {
             $va =   '<center>
                         <a style="text-transform:uppercase;" href="' . $getData->kode_bayar_1 . '"  class="btn btn-primary">Bayar Sekarang</a>
                     </center>';
-            $pesan = "Silahkan Lengkapi Pembayaran Sebelum <br><strong>".$getData2->updated_at->format('l, d M Y')."</strong> pukul </strong>".$getData->updated_at->addMinutes(15)->format('H:i')."</strong>";
+            $pesan = "Silahkan Lengkapi Pembayaran Sebelum <br><strong>" . $getData2->updated_at->format('l, d M Y') . "</strong> pukul </strong>" . $getData->updated_at->addMinutes(15)->format('H:i') . "</strong>";
         }
         session()->forget('apply');
         Session::flash('popupAfterMentoring', [
             'kode' => $va,
-            'pesan' => $pesan 
+            'pesan' => $pesan
         ]);
-        return Redirect::to('/'.$ref_transaction_layanan.'/notification-mentoring/success');
+        return Redirect::to('/' . $ref_transaction_layanan . '/notification-mentoring/success');
     }
 
 
@@ -209,13 +203,13 @@ class MentoringTransaksiController extends Controller
     {
         $tabelPembayaran = PembayaranLayanan::where('ref_transaction_layanan', $ref)->pluck('id')->first();
         $diskon = Voucher::where('id', $voucher_id)->first();
-        $voucher_id == null ? $potongan = 0 : $potongan = $diskon->jumlah_diskon; 
+        $voucher_id == null ? $potongan = 0 : $potongan = $diskon->jumlah_diskon;
         $data = PembayaranLayanan::with('user', 'paket_non_professionals.layanan_non_professionals', 'psikolog', 'detail_pembayarans')->where('ref_transaction_layanan', $ref)->first();
         // -----------------BNI, BRI, BCA, CIMB----------------------- //
         if ($bank == 'bni' || $bank == 'bri' || $bank == 'bca' || $bank == 'cimb') {
             $data = [
                 "reference" => $ref,
-                "harga_event" => $data->paket_non_professionals->harga - $potongan ,
+                "harga_event" => $data->paket_non_professionals->harga - $potongan,
                 "nama"  => $data->user->nama,
                 "email"  => $data->user->email,
                 "no_tlpn" => $data->user->no_whatsapp
@@ -247,7 +241,7 @@ class MentoringTransaksiController extends Controller
                 "status_code" => $res["status_code"],
                 "message" =>  $res["status_message"],
             ];
-            return $responData;
+            return $res;
         }
         // -----------------MANDIRI----------------------- //
         else if ($bank == "mandiri") {
@@ -285,7 +279,7 @@ class MentoringTransaksiController extends Controller
                 "status_code" => $res["status_code"],
                 "message" =>  $res["status_message"],
             ];
-            return $responData;
+            return $res;
         }
         // -----------------PERMATA----------------------- //
         else if ($bank == "permata") {
@@ -323,7 +317,7 @@ class MentoringTransaksiController extends Controller
                 "status_code" => $res["status_code"],
                 "message" => $res["status_message"],
             ];
-            return $responData;
+            return $res;
         }
         // -----------------INDOMARET----------------------- //
         else if ($bank == "indomaret") {
@@ -343,7 +337,7 @@ class MentoringTransaksiController extends Controller
                     'status_code' => $res["status_code"],
                     'message' => $res['status_message']
                 ];
-                return $responseError;
+                return $res;
             }
             // insert db
             PembayaranLayanan::where('ref_transaction_layanan', $res["order_id"])->update([
@@ -362,7 +356,7 @@ class MentoringTransaksiController extends Controller
                 "status_code" => $res["status_code"],
                 "message" =>  $res["status_message"],
             ];
-            return $responData;
+            return $res;
         }
         // -----------------ALFAMART----------------------- //
         else if ($bank == "alfamart") {
@@ -400,8 +394,8 @@ class MentoringTransaksiController extends Controller
                 "status_code" => $res["status_code"],
                 "message" =>  $res["status_message"],
             ];
-            return $responData;
-        } 
+            return $res;
+        }
         // ---------GOPAY---------------
         else if ($bank == "gopay") {
             $data = [
@@ -437,7 +431,7 @@ class MentoringTransaksiController extends Controller
                 "status_code" => $res["status_code"],
                 "message" =>  $res["status_message"],
             ];
-            return $responData;
+            return $res;
         }
         // ----------------------------SHOPEEPAY--------------------- //-----------------------
         else if ($bank == "shopeepay") {
@@ -476,7 +470,7 @@ class MentoringTransaksiController extends Controller
                 "status_code" => $res["status_code"],
                 "message" =>  $res["status_message"],
             ];
-            return $responData;
+            return $res;
         }
     }
 }
